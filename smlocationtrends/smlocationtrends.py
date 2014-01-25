c929ddbf19007d9467dfb8a0ef6e719dc342217f
rjduffner <rjduffner@gmail.com>
2014-01-24 22:41:47 -0800

    merge into pyramid

--- smlocationtrends/smlocationtrends.py
+++ smlocationtrends/smlocationtrends.py
@@ -8,6 +8,7 @@ smlocationtrends.py
 
 """
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 
 from surveyinformation import SurveyInformation
 from surveyresults import SurveyResults
@@ -19,19 +20,22 @@ SM_ACCESS_TOKEN='UFHR1aBDl2QjFoOzyDhoj91aM1Q3Atp-HtOvcI8kBk.HIBEdrGLtGKLnbSmHGcE
 
 @view_config(route_name='home', renderer='templates/index.jinja2')
 def index(request):
-    print dir(request)
-    '''
-    if 'survey_id' in request.args.keys():
-        return redirect(url_for('survey', survey_id=request.args['survey_id']))
-    '''
-    #return render_template('index.jinja2')
+    params = dict(request.GET)
+    if 'survey_id' in params:
+        return HTTPFound(location='/survey/'+ request.GET['survey_id'])
     return {'project': 'smlocationtrends'}
 
 @view_config(route_name='survey', renderer='templates/survey.jinja2')
 def survey(request):
+    survey_id = request.matchdict['survey_id']
+    print survey_id
     si = SurveyInformation(SM_API_KEY, SM_ACCESS_TOKEN)
     pages, question_dict = si.get_survey_page_count_and_questions(survey_id)
-    return render_template('survey.jinja2', pages=pages, questions=question_dict, survey_id=survey_id)
+    return {'project': 'smlocationtrends',
+            'pages': pages,
+            'questions': questions,
+            'survey_id': survey_id
+            }
 
 @view_config(route_name='trends', renderer='templates/trends.jinja2')
 def survey_information(request):
